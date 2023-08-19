b73ee2e382c927e7b7c3ce61d10769ecd93bbc9e
Nandhinimoha <138390550+Nandhinimoha@users.noreply.github.com>
2023-08-19 20:40:51 +0530

    Task_3.py (DDF)

--- Event/Task_3.py
+++ Event/Task_3.py
@@ -7,16 +7,12 @@ from Event.excel_function import Healthcare
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
-
+# Data driven framework
 file = "C:\\Users\\NANDHU-PC HOME\\PycharmProjects\\pythonProject\\PageObjectModel\\Event\\test_data1.xlsx"
 sheet_number = 'Sheet1'
 s = Healthcare(file, sheet_number)
-
-
 rows = s.row_count()
-print(rows)
-
-
+print(rows)  # print Number of rows in excel file
 try:
 
     for row in range(2, rows+1):
@@ -33,33 +29,22 @@ try:
         driver.implicitly_wait(10)
         wait = WebDriverWait(driver, 20)
 
-        # driver.find_element(By.XPATH, locators.Locators().Make_appointment).click()
-
         wait.until(EC.element_to_be_clickable((By.XPATH,locators.Locators().Make_appointment))).click()
-        cookie_before = driver.get_cookies()[0]['value']
         wait.until(EC.presence_of_element_located((By.XPATH, locators.Locators().username))).send_keys(username)
         wait.until(EC.presence_of_element_located((By.XPATH, locators.Locators().password))).send_keys(password)
         wait.until(EC.element_to_be_clickable((By.XPATH, locators.Locators().login_btn))).click()
 
-
-        after_url = driver.current_url
-        cookie_after = driver.get_cookies()[0]['value']
-
-
+    # Book appointment validation
 
         if "https://katalon-demo-cura.herokuapp.com/#appointment" in driver.current_url:
             for i in range(2, rows):
                 comments = s.read_data(i,5)
                 wait.until(EC.presence_of_element_located((By.XPATH,locators.Locators().comments))).send_keys(comments)
                 break
-
-
             wait.until(EC.element_to_be_clickable((By.XPATH, locators.Locators().check_box))).click()
             wait.until(EC.element_to_be_clickable((By.XPATH, locators.Locators().date))).click()
             wait.until(EC.presence_of_element_located((By.XPATH, locators.Locators().date_select))).click()
-
             wait.until(EC.element_to_be_clickable((By.XPATH, locators.Locators().book_appointment))).click()
-
             print('SUCCESS : login with {a}'.format(a=username))
             print("Appointment booked!!")
             s.write_data(row, 8, "TEST PASS")
